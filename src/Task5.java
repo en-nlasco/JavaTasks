effa4e1bc9f5fd8c37da5a40c04c8a49a9237e0f
Natalia Lasco <natalia.lasco@endava.com>
2020-05-04 17:58:12 +0300

    Task5

---
+++ src/Task5.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int arrayLength;
+        int evenAmount=0;
+        Integer oddSum=0;
+        boolean changesDone = false;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please specify the lengths of your array of Integers");
+        arrayLength = in.nextInt();
+        Integer[] array = new Integer[arrayLength];
+        System.out.println("Please insert " + arrayLength + " elements:");
+        for (int i = 0; i < arrayLength; i++)
+            array[i] = in.nextInt();
+        for(Integer i:array)
+            if (i%2==0)
+                evenAmount++;
+            else oddSum=oddSum+i;
+        System.out.println("Sum of the odd numbers is: "+oddSum);
+        System.out.println("Amount of the even numbers is: "+evenAmount);
+    }
+}
