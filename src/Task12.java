1d690bef71da7acdcc14d0683bf73abfa8e5252f
Natalia Lasco <natalia.lasco@endava.com>
2020-05-19 19:41:08 +0300

    Task12

---
+++ src/Task12.java
@@ -0,0 +1,56 @@
+package src;
+
+
+import java.util.Scanner;
+
+
+public class Task12 {
+    //Given a matrix of integers with size n*n, sort the lines of the matrix by the sum of the elements on those lines
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Integer[][] matrix;
+        System.out.println("Please insert matrix size");
+        int n = in.nextInt();
+        matrix = new Integer[n][n];
+        System.out.println("Now, please insert elements of the matrix");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = in.nextInt();
+
+        System.out.println("Here is yout matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(matrix[i][j] + ", ");
+            System.out.println();
+        }
+
+        sortMatrixLines(matrix);
+        System.out.println("Here is result after sorting the lines:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(matrix[i][j] + ", ");
+            System.out.println();
+        }
+    }
+
+    public static void sortMatrixLines(Integer[][] matrix) {
+        Integer[] line = new Integer[matrix[0].length];
+        boolean changesMade = false;
+        do {
+            for (int i = 0; i < matrix[0].length - 1; i++) {
+                changesMade = false;
+                Integer sum1 = 0, sum2 = 0;
+                for (Integer m : matrix[i])
+                    sum1 += m;
+                for (Integer m : matrix[i + 1])
+                    sum2 += m;
+                if (sum2 < sum1) {
+                    line = matrix[i + 1];
+                    matrix[i + 1] = matrix[i];
+                    matrix[i] = line;
+                    changesMade = true;
+                }
+            }
+        } while (changesMade);
+    }
+}
